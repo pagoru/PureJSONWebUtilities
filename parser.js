e9d3a0b7503cde6c5c5f177f577999131ce0bceb
pagoru <pagoru@gmail.com>
2016-10-11 17:17:10 +0200

    Fixed #2

--- parser.js
+++ parser.js
@@ -1,116 +1,159 @@
 /**
  * Created by pablo on 10/10/16.
  */
-module.exports = {
-    hton: function(data) {
+function parseSSONContent(data, prev, variables){
 
-        var rawHtml = "";
+    prev = prev == null ? "" : prev;
+    var rawCss = "";
 
-        for (var i = 0; i < data.length; i++) {
+    for(var i = 0; i < data.length; i++){
 
-            var rawAttributes = "";
-            var tag = data[i]['tag'];
-            var content = data[i]['content'];
-            var attributes = data[i]['attributes'];
+        if(data[i]['tag'].startsWith("@:")){
+            var d = variables[data[i]['tag'].replace('@:', '')];
+            if(d instanceof Object){
+                data[i] = d;
+            }
+        }
+        var tag = data[i]['tag'];
+        var attributes = data[i]['attributes'];
+        var selectors = data[i]['selectors'];
+        var children = data[i]['children'];
 
-            if (attributes != null) {
+        if(attributes != null){
+            rawCss += prev + tag + "{";
 
-                var keysAttr = Object.keys(attributes);
+            var keysAttr = Object.keys(attributes);
+            for (var s = 0; s < keysAttr.length; s++) {
+                var kAt = keysAttr[s];
+                var vAt = attributes[kAt] + "";
 
-                for (var s = 0; s < keysAttr.length; s++) {
-                    var kAt = keysAttr[s];
-                    var vAt = attributes[kAt];
-                    rawAttributes += " ";
+                if(vAt.startsWith("@:")){
+                    var v = variables[vAt.replace('@:', '')];
+                    if(!(v instanceof Object)){
+                        vAt = v;
+                    }
+                }
 
-                    switch (kAt) {
-                        case 'style':
-                            rawAttributes += "style=\"";
+                if(typeof vAt == 'number'){
+                    vAt += 'px';
+                }
 
-                            var rawStyle = "";
-                            var keysStyle = Object.keys(vAt);
+                rawCss += kAt + ": " + vAt + ";";
 
-                            for (var s = 0; s < keysStyle.length; s++) {
-                                var kSt = keysStyle[s];
-                                var vSt = vAt[kSt];
+            }
 
-                                if (typeof vSt == 'number') {
-                                    vSt += "px";
-                                }
+            rawCss += "} ";
+        }
 
-                                rawStyle += kSt + ": " + vSt + ";";
+        if(children != null){
+            rawCss += parseSSONContent(children, prev + tag + " ", variables);
+        }
+
+        if(selectors != null){
+            rawCss += parseSSONContent(selectors, prev + tag + ":", variables);
+        }
+
+    }
+
+    return rawCss;
+
+}
+function parseHTONContent(data, variables){
+
+    var rawHtml = "";
+
+    for (var i = 0; i < data.length; i++) {
+
+        var rawAttributes = "";
+
+        if(data[i]['tag'].startsWith("@:")){
+            data[i] = variables[data[i]['tag'].replace('@:', '')]
+        }
+
+        var tag = data[i]['tag'];
+        var content = data[i]['content'];
+        var attributes = data[i]['attributes'];
+
+        if (attributes != null) {
+
+            var keysAttr = Object.keys(attributes);
+
+            for (var s = 0; s < keysAttr.length; s++) {
+                var kAt = keysAttr[s];
+                var vAt = attributes[kAt];
+                rawAttributes += " ";
+
+                switch (kAt) {
+                    case 'style':
+                        rawAttributes += "style=\"";
+
+                        var rawStyle = "";
+                        var keysStyle = Object.keys(vAt);
+
+                        for (var s = 0; s < keysStyle.length; s++) {
+                            var kSt = keysStyle[s];
+                            var vSt = vAt[kSt];
+
+                            if (typeof vSt == 'number') {
+                                vSt += "px";
                             }
 
-                            rawAttributes += rawStyle + "\"";
-                            break;
-                        default:
-                            rawAttributes += kAt + "=\"" + vAt + "\"";
-                            break;
-                    }
+                            rawStyle += kSt + ": " + vSt + ";";
+                        }
 
+                        rawAttributes += rawStyle + "\"";
+                        break;
+                    default:
+                        rawAttributes += kAt + "=\"" + vAt + "\"";
+                        break;
                 }
 
             }
 
-            if (content == null) {
-                rawHtml += "<" + tag + rawAttributes + "/>";
-                continue;
-            }
-
-            var rawContent = "";
-
-            if (content instanceof Object) {
-                rawContent += this.hton(content);
-            } else {
-                rawContent += content;
-            }
-
-            rawHtml += "<" + tag + rawAttributes + ">" + rawContent + "</" + tag + ">";
-
         }
-        return rawHtml;
+
+        if (content == null) {
+            rawHtml += "<" + tag + rawAttributes + "/>";
+            continue;
+        }
+
+        var rawContent = "";
+
+        if (content instanceof Object) {
+            rawContent += parseHTONContent(content, variables);
+        } else {
+            rawContent += content;
+        }
+
+        rawHtml += "<" + tag + rawAttributes + ">" + rawContent + "</" + tag + ">";
+
+    }
+    return rawHtml;
+}
+
+function getVariables(variables){
+    var vars = [];
+
+    if(variables != null){
+        var keysVariables = Object.keys(variables);
+
+        for (var i = 0; i < keysVariables.length; i++) {
+            var keyVariable = keysVariables[i];
+            var valueVariable = variables[keyVariable];
+
+            vars[keyVariable] = valueVariable;
+        }
+    }
+    return vars;
+}
+
+module.exports = {
+    hton: function(data) {
+        return parseHTONContent(data.content, getVariables(data.variables));
     },
 
-    sson: function(data, prev){
+    sson: function(data){
+        return parseSSONContent(data.content, "", getVariables(data.variables));
 
-        prev = prev == null ? "" : prev;
-        var rawCss = "";
-
-        for(var i = 0; i < data.length; i++){
-
-            var tag = data[i]['tag'];
-            var attributes = data[i]['attributes'];
-            var selectors = data[i]['selectors'];
-            var children = data[i]['children'];
-
-            if(attributes != null){
-                rawCss += prev + tag + "{";
-
-                var keysAttr = Object.keys(attributes);
-                for (var s = 0; s < keysAttr.length; s++) {
-                    var kAt = keysAttr[s];
-                    var vAt = attributes[kAt];
-
-                    if(typeof vAt == 'number'){
-                        vAt += 'px';
-                    }
-
-                    rawCss += kAt + ": " + vAt + ";";
-
-                }
-
-                rawCss += "} ";
-            }
-
-            if(children != null){
-                rawCss += this.sson(children, prev + tag + " ");
-            }
-
-            if(selectors != null){
-                rawCss += this.sson(selectors, prev + tag + ":");
-            }
-
-        }
-
-        return rawCss;
     }
 }
--- server.js
+++ server.js
@@ -3,35 +3,17 @@
  */
 const ROOT_PATH = '/Users/pablo/Documents/i-workspace/php/nodeJsonToHtml/';
 const http = require('http');
-const util = require('util');
 const parser = require('./parser.js');
 var fs = require('fs');
 
-const hostname = '127.0.0.1';
-const port = 8080;
+var web = JSON.parse(fs.readFileSync(ROOT_PATH + 'web.json', 'utf8'));
 
-const server = http.createServer((req, res) => {
+var header = web.header['content-type'];
+var content = parser.hton(web.hton);
+var cssContent = parser.sson(web.sson);
 
-    var web = JSON.parse(fs.readFileSync(ROOT_PATH + 'web.json', 'utf8'));
+console.log(header);
+console.log(content);
+console.log(cssContent);
 
-    var header = web.header['content-type'];
-    var content = parser.hton(web.content);
-    var cssContent = parser.sson(web.stylesheet);
-
-    res.statusCode = 200;
-    if(req.url.includes("css")){
-        res.setHeader('Content-Type', "text/css");
-        res.write(cssContent);
-        res.end();
-        return;
-    }
-    res.setHeader('Content-Type', header);
-    res.write(content);
-    res.end();
-
-    console.log(req.headers.host + " connected.");
-});
-
-server.listen(port, hostname, () => {
-    console.log(`El servidor se está ejecutando en http://${hostname}:${port}/`);
-});
+// console.log(web.hton.variables);
