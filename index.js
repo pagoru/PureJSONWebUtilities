b43437d03beb27ce7fc03135067f365b615882f3
pagoru <pagoru@gmail.com>
2016-10-11 22:38:47 +0200

    Fixed #4

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
 /**
  * Created by pablo on 10/10/16.
  */
-const ROOT_PATH = '/Users/pablo/Documents/i-workspace/php/nodeJsonToHtml/';
+const fs = require('fs');
+const ROOT_PATH = fs.realpathSync('.') + '/';
 const http = require('http');
 const parser = require('./parser.js');
-var fs = require('fs');
 
 var web = JSON.parse(fs.readFileSync(ROOT_PATH + 'web.json', 'utf8'));
 
